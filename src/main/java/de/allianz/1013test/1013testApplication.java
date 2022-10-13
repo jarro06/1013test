b7083ee79eef20d2ee4d98fe55112620d8b6b96d
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-10-13 10:19:01 +0200

    Create maven project

---
+++ src/main/java/de/allianz/1013test/1013testApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.1013test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class 1013testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(1013testApplication.class, args);
+	}
+
+}
